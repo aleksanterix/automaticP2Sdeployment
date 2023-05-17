9f7344a2d45fe2658877ff7ddcb19a2c226ea0f0
aleksanterix <aleksanteri.halonen@avanio.com>
2023-05-17 15:56:50 +0300

    third commit

--- run.ps1
+++ run.ps1
@@ -20,13 +20,9 @@
 #     [string]$location
 # )
 
-##! Variables
-$rootCertName = "P2SRootCertTEST"
-$certName = "P2SChildCertTEST"
-$password = ConvertTo-SecureString -String "password123" -Force -AsPlainText
-
-##! Functions
-#Function to get the certificate thumbprint in order to get the certificate path
+###! Functions
+##! Certificate functions
+# Get certificate thumbprint
 function Get-Cert-Thumbprint {
     param (
         $Subject
@@ -34,132 +30,255 @@ function Get-Cert-Thumbprint {
     return Get-ChildItem Cert:\CurrentUser\My\ | Where-Object -FilterScript { $_.Subject -eq "CN=$Subject"} | Select-Object -ExpandProperty Thumbprint
 }
 
-
-##! Generate root certificate and client certificate, and export them to a base64 file in the current directory
-# Generate root certificate
-Write-Host "Generating root certificate..."
-$cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
--Subject "CN=$rootCertName" -KeyExportPolicy Exportable `
--HashAlgorithm sha256 -KeyLength 2048 `
--CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign
-
-# Generate client certificate signed by root certificate
-Write-Host "Generating client certificate..."
-New-SelfSignedCertificate -Type Custom -DnsName $certName -KeySpec Signature `
--Subject "CN=$certName" -KeyExportPolicy Exportable `
--HashAlgorithm sha256 -KeyLength 2048 `
--CertStoreLocation "Cert:\CurrentUser\My" `
--Signer $cert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
-
-# Define the certificate path
-$certThumbprint = Get-Cert-Thumbprint $rootCertName
-$certPath = "Cert:\CurrentUser\My\$certThumbprint" 
-$exportPath = Join-Path -Path $pwd -ChildPath "$rootCertName.cer"
-$base64Cert = $rootCertName + '64' + '.cer'
-$export64Path = Join-Path -Path $pwd -ChildPath $base64Cert
-
-# Export the root certificate to a file and convert it to base64
-Write-Host "Exporting root certificate to a file..."
-Export-Certificate -Cert $certPath -FilePath $exportPath -Type CERT
-certutil.exe -encode $exportPath $export64Path
-
-# Remove the non-base64 certificate file
-Write-Host "Cleaning up..."
-Remove-Item $exportPath
-
-## Parse the base64 certificate file to a variable and remove -----BEGIN CERTIFICATE----- and -----END CERTIFICATE----- from the string
-# Read the certificate file content
-$certFile = Get-Content -Path $export64Path
-# Remove the BEGIN and END certificate lines
-$certFile = $certFile | Where-Object { $_ -ne '-----BEGIN CERTIFICATE-----' -and $_ -ne '-----END CERTIFICATE-----' }
-$certString = $certFile -join ''
-
-
-#####! Azure part
-
-Write-Host "Connecting to Azure..."
-Write-Host "You will be prompted to login to Azure."
-
-#! Connecting to Azure and setting the subscription
-Connect-AzAccount
-
-# Get subscriptions
-$subscriptions = Get-AzSubscription
-
-# Present the list of subscriptions to the user and prompt for selection
-Write-Host "Available subscriptions: "
-$subscriptionChoiches = @()
-# Loop through the subscriptions, add to array and present them to the user
-$index = 0
-foreach ($subscription in $subscriptions) {
-    Write-Host "$index. $($subscription.Name)"
-    $subscriptionChoiches += $subscription.Id
-    $index++
+function Get-Cert-Path {
+    param (
+        $Subject
+    ) 
+    return "Cert:\CurrentUser\My\" + (Get-Cert-Thumbprint $Subject)
 }
-# Prompt for selection
-$selection = Read-Host "Enter the number corresponding to the desired subscription"
-$subscriptionId = $subscriptionChoiches[$selection]
 
-Write-Host "Setting subscription to $subscriptionId"
+# Check if the certificate exists by invoking the Get-Cert-Thumbprint command. If the value is null, the certificate does not exist.
+function Test-Cert-Exists {
+    $rootCertThumbprint = Get-Cert-Thumbprint $rootCertName
+    $certThumbprint = Get-Cert-Thumbprint $certName
 
-Set-AzContext -SubscriptionId $subscriptionId
-
-Write-Host "Subscription set successfully."
-
-
-#! Get location for deployments
-# Get locations
-Write-Host "Getting available locations for deployments..."
-$locations = Get-AzLocation | Where-Object {$_.Providers -contains "Microsoft.AppConfiguration"}
-
-# Present the list of locations to the user and prompt for selection
-Write-Host "Available locations: "
-$index = 0
-$locationChoiches = @()
-foreach ($location in $locations) {
-    Write-Host "$index. $($location.DisplayName)"
-    $locationChoiches += $location.DisplayName
-    $index++
+    Write-Host  $rootCertName
+    Write-Host  $certName
+    # Check if the root certificate exists
+    if ($null -eq $rootCertThumbprint -or $null -eq $certThumbprint) {
+        return $false
+    }
+    else {
+        return $true
+    }
 }
-# Prompt for selection
-$selection = Read-Host "Enter the number corresponding to the desired location"
-$location = $locationChoiches[$selection]
 
-Write-Host "Location set to $location. All resources will be deployed to this location."
+# Generate root certificate and client certificate
+function New-Certificates {
+    param ()
+    # Generate root certificate
+    Write-Host "Generating root certificate..."
+    $cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
+    -Subject "CN=$rootCertName" -KeyExportPolicy Exportable `
+    -HashAlgorithm sha256 -KeyLength 2048 `
+    -CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign
 
-#! Get resource group, or create it if it doesn't exist
+    # Generate client certificate signed by root certificate
+    Write-Host "Generating client certificate..."
+    New-SelfSignedCertificate -Type Custom -DnsName $certName -KeySpec Signature `
+    -Subject "CN=$certName" -KeyExportPolicy Exportable `
+    -HashAlgorithm sha256 -KeyLength 2048 `
+    -CertStoreLocation "Cert:\CurrentUser\My" `
+    -Signer $cert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
+}
+# Export root certificate to a Base64 file
+function Set-CertificateToBase64File {
+    param ()
+    # Define the certificate path
+    $certThumbprint = Get-Cert-Thumbprint $rootCertName
+    $certPath = "Cert:\CurrentUser\My\$certThumbprint" 
+    $exportPath = Join-Path -Path $pwd -ChildPath "$rootCertName.cer"
+    $base64Cert = $rootCertName + '64' + '.cer'
+    $export64Path = Join-Path -Path $pwd -ChildPath $base64Cert
 
-# Ask for resource group name
+    # Export the root certificate to a file and convert it to base64
+    Write-Host ""
+    Write-Host "Exporting root certificate to a file..."
+    Export-Certificate -Cert $certPath -FilePath $exportPath -Type CERT > $null
+    certutil.exe -encode $exportPath $export64Path > $null
+    Write-Host "Root certificate exported to $export64Path"
 
-Write-Host "Please enter the name of the resource group to deploy to. If it doesn't exist, it will be created."
-$rg = Read-Host "Enter the name of the resource group to deploy to"
+    # Remove the non-base64 certificate file
+    Write-Host "Cleaning up..."
+    Remove-Item $exportPath
+    Write-Host "Cleanup complete."
+    Write-Host "Certificate export complete."
+    Write-Host ""
 
-# Check if resource group exists
-$rgExists = Get-AzResourceGroup -Name $rg -ErrorAction SilentlyContinue
+    return $export64Path
 
-# If resource group doesn't exist, create it
-if (!$rgExists) {
-    $answer = Read-Host "Resource group $rg does not exist. Would you like to create it? (y/n)"
+}
+# Get the base64 string from the certificate file
+function Get-Cert-Base64String {
+    param ()
+    ## Parse the base64 certificate file to a variable and remove -----BEGIN CERTIFICATE----- and -----END CERTIFICATE----- from the string
+    # Read the certificate file content
+    $certFile = Get-Content -Path $export64Path
+    # Remove the BEGIN and END certificate lines
+    $certFile = $certFile | Where-Object { $_ -ne '-----BEGIN CERTIFICATE-----' -and $_ -ne '-----END CERTIFICATE-----' }
+    $certString = $certFile -join ''
+
+    return $certString
+}
+##! Azure functions
+# Set the subscription
+function Set-ValueForSubscription {
+    param ()
+    # Get subscriptions
+    $subscriptions = Get-AzSubscription
+
+    # Present the list of subscriptions to the user and prompt for selection
+    Write-Host "Available subscriptions: "
+    
+    # Create an array to store the subscription IDs
+    $subscriptionChoiches = @()
+
+    # Loop through the subscriptions, add to array and present them to the user
+    $index = 0
+    foreach ($subscription in $subscriptions) {
+        Write-Host "$index. $($subscription.Name)"
+        $subscriptionChoiches += $subscription.Id
+        $index++
+    }
+    # Prompt for selection
+    $selection = Read-Host "Enter the number corresponding to the desired subscription"
+    $subscriptionId = $subscriptionChoiches[$selection]
+
+    Write-Host "Setting subscription to $subscriptionId"
+
+    Set-AzContext -SubscriptionId $subscriptionId
+
+    Write-Host "Subscription set successfully."
+}
+function Set-ValueForLocation {
+    param ()
+    # Get locations
+    Write-Host "Getting available locations for deployments..."
+    $locations = Get-AzLocation | Where-Object {$_.Providers -contains "Microsoft.AppConfiguration"}
+
+    # Present the list of locations to the user and prompt for selection
+    Write-Host "Available locations: "
+    $index = 0
+    $locationChoiches = @()
+    foreach ($location in $locations) {
+        Write-Host "$index. $($location.DisplayName)"
+        $locationChoiches += $location
+        $index++
+    }
+    # Prompt for selection
+    $selection = Read-Host "Enter the number corresponding to the desired location"
+    $location = $locationChoiches[$selection]
+
+    Write-Host "Location set to "$location.DisplayName" All resources will be deployed to this location."
+
+    return $location 
+}
+
+function Set-ValueForResourceGroup {
+    param ()
+    # Ask for resource group name
+
+    Write-Host "Please enter the name of the resource group to deploy to. If it doesn't exist, it will be created."
+    $rg = Read-Host "Enter the name of the resource group to deploy to"
+
+    # Check if resource group exists
+    $rgExists = Get-AzResourceGroup -Name $rg -ErrorAction SilentlyContinue
+
+    # If resource group doesn't exist, create it
+    if (!$rgExists) {
+        $answer = Read-Host "Resource group $rg does not exist. Would you like to create it? (y/n)"
+        if ($answer -eq "y") {
+            Write-Host "Creating resource group $rg in location "$location.DisplayName""
+            New-AzResourceGroup -Name $rg -Location $location.Location > $null
+            Write-Host ""
+            Write-Host "Resource group $rg created successfully."
+            Write-Host ""
+        } else {
+            Write-Host "Exiting..."
+            exit
+        }
+    } else {
+        Write-Host "Resource group $rg exists. Continuing..."
+    }
+    return $rg
+}
+
+#####! Main script !#####
+
+## Greeting and instructions
+Write-Host "Welcome to the VPN deployment script."
+Write-Host "This script will generate root and client certificates, deploy the VPN gateway and VPN client VMs, and connect to the VPN gateway."
+Write-Host "Please follow the instructions."
+Write-Host ""
+Write-Host "First, I need you to give a name that will be used to generate the names for the certificates. Please enter a unique name for the certificates."
+Write-Host "For example, if you enter 'test', the root certificate will be named 'testRootCert' and the client certificate will be named 'testChildCert'."
+Write-Host ""
+Write-Host "After this is done and the certificates are generated, the script will authenticate with Azure."
+Write-Host "Then, the script will ask for you to select the subscription, location and resource group."
+Write-Host "The script will then deploy the VPN gateway and VPN client VMs using the ARM template."
+Read-Host "Press Enter to continue..."
+Write-Host ""
+
+## Ask for values
+$certNaming = Read-Host "Enter the name for the certificates"
+$rootCertName = $certNaming + "RootCert"
+$certName = $certNaming + "ChildCert"
+
+# Check if a certificate with the same name exists. And ask if the user wants to delete the existing certificates
+$certExists = Test-Cert-Exists
+
+if (!$certExists) {
+    Write-Host "Certificate $rootCertName or $certName do not exist."
+    Write-Host "Continuing..."
+} else {
+    Write-Host "Certificate $rootCertName or $certName already exist."
+    $answer = Read-Host "Would you like to delete them? (y/n)"
     if ($answer -eq "y") {
-        Write-Host "Creating resource group $rg in location $location"
-        New-AzResourceGroup -Name $rg -Location $location
+        $rootThumb = Get-Cert-Thumbprint -Subject $rootCertName
+        $childThumb = Get-Cert-Thumbprint -Subject $certName
+        $rootPath = "Cert:\CurrentUser\My\" + $rootThumb
+        $childPath = "Cert:\CurrentUser\My\" + $childThumb
+        Write-Host "Deleting certificates..."
+        Remove-Item -Path $rootPath
+        Remove-Item -Path $childPath
+        Write-Host "Certificates deleted."
     } else {
         Write-Host "Exiting..."
         exit
     }
-} else {
-    Write-Host "Resource group $rg exists. Continuing..."
 }
+
+###! Certificate part !###
+
+# Generate root certificate and client certificate, and export them to a base64 file in the current directory then get the base64 string to a variable
+
+New-Certificates
+
+$export64Path = Set-CertificateToBase64File
+
+$CertString = Get-Cert-Base64String
+
+##### Azure part #####
+
+Write-Host "Connecting to Azure..."
+Write-Host "You will be prompted to login to Azure."
+
+# Connect to Azure
+Connect-AzAccount
+
+# Set the subscription
+
+Set-ValueForSubscription
+
+# Get location for deployments
+
+$location = Set-ValueForLocation
+
+# Get resource group, or create it if it doesn't exist
+
+$rg = Set-ValueForResourceGroup
+
+
 
 #! Gather parameters for ARM Template deployment
 
 $parameters = @{
-    "Location" = $location
+    "Location" = $location.Location
     "vpnClientRootCertPublicCertData" = $certString
 }
 
 #! Deploy ARM Template
 
-Read-Host "We will now deploy the ARM Template. Press Enter to continue or CTRL+C to cancel."
+Read-Host "We will now deploy the ARM Template. Press Enter to continue or CTRL+C to cancel"
 
-New-AzResourceGroupDeployment -ResourceGroupName "$rg" -TemplateParameterObject $parameters
+# New-AzResourceGroupDeployment -ResourceGroupName "$rg" -TemplateParameterObject $parameters
